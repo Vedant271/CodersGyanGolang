15ea5059cd192d2f4ce669ec38e774a19608abee
Vedant Karale <vedantkarale271@gmail.com>
2026-09-05 20:29:39 +0530

    simple values and variables

---
+++ 2_simple_values/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Golang")
+
+	fmt.Println(2)
+
+	fmt.Println(2 + 2)
+
+	fmt.Println(true)
+
+	fmt.Println(false)
+
+	fmt.Println(50.3)
+
+	fmt.Println(7 / 3)
+
+	fmt.Println(7 / 3.0)
+
+	fmt.Println(7.0 / 3)
+
+	fmt.Println(7.0 / 3.0)
+}
---
+++ 3_variables/variable.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+	// var name = "golang 2"
+	// name := "golang 3"
+
+	var name string
+
+	name = "golang 4"
+
+	fmt.Println(name)
+
+}
